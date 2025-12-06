ab02e0e20c6b90a7a9365a5770c93429d0239869
Estrella6066 <3542515926@qq.com>
2025-12-06 15:13:57 +0800

    修复错误的显示问题

--- src/main/java/org/est/ConditionalPatternLayout.java
+++ src/main/java/org/est/ConditionalPatternLayout.java
@@ -2,6 +2,7 @@ package org.est;
 
 import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.pattern.PatternLayoutEncoderBase;
 
 public class ConditionalPatternLayout extends PatternLayout {
     
@@ -35,5 +36,16 @@ public class ConditionalPatternLayout extends PatternLayout {
         // 将时间戳转换为可读格式：yyyy-MM-dd HH:mm:ss.SSS
         java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         return sdf.format(new java.util.Date(timestamp));
+    }
+    
+    // 重写start方法以确保正确初始化
+    @Override
+    public void start() {
+        // 确保pattern不为空
+        if (getPattern() == null || getPattern().isEmpty()) {
+            // 设置一个默认pattern，即使我们重写了doLayout方法
+            setPattern("%d{HH:mm:ss} %-5level %logger{36} - %msg%n");
+        }
+        super.start();
     }
 }
